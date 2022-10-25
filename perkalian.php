3205f061df20ae41645cde075dd7fc92ae33c06e
yuanbntng <116611384+yuanbntng@users.noreply.github.com>
2022-10-25 08:42:51 +0700

    Add files via upload
    NAMA ; MUHAMMAD YUAN BINTANG
    NIM  ; 201410144
    KELAS ; SI5D
    MATAKULIAH ; RPL

---
+++ perkalian.php
@@ -0,0 +1,25 @@
+<?php
+echo "<b> quis : operator perkalian 2 </b><p>";
+
+$a=5*1;
+$b=5*2;
+$c=5*3;
+$d=5*4;
+$e=5*5;
+$f=5*6;
+$g=5*7;
+$h=5*8;
+$i=5*9;
+$j=5*10;
+
+echo "5 x 1= ".$a;
+echo "<br>5 x 2= ".$b;
+echo "<br>5 x 3= ".$c;
+echo "<br>5 x 4= ".$d;
+echo "<br>5 x 5= ".$e;
+echo "<br>5 x 6= ".$f;
+echo "<br>5 x 7= ".$g;
+echo "<br>5 x 8= ".$h;
+echo "<br>5 x 9= ".$i;
+echo "<br>5 x 10= ".$j;
+?>
